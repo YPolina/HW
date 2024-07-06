13d6ec1d492c2b08be307317430e0a42149a12af
Polina <yatsko_polina1@mail.ru>
2024-07-06 22:36:47 +0300

    Дополнения код + Файл с зависимостями

--- hw1.py
+++ hw1.py
@@ -5,6 +5,11 @@ while True:
         number = int(input("Введите целое число: "))
     except ValueError:
         print('Неверный ввод! Необходимо ввести целое число')
+        decision = input('Желаете продолжить? Y/N\n')
+        if decision in ['Y', 'y', 'Yes', 'yes']:
+            continue
+        else:
+            break
     else:
         print(pow(number, 2))
         break
